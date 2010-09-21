7f051a7c657e51252477b4b0252227a335073a6d
Thiago Bastos <tabastos@gmail.com>
2010-09-21 02:35:24 -0300

    Oops, mixing up Lua with C++.

--- src/launcher/Main.cpp
+++ src/launcher/Main.cpp
@@ -339,7 +339,7 @@ int main( int argc, char* argv[] )
 			// if the result is a number, use it as the return status; otherwise, print it
 			if( res.isValid() )
 			{
-				if( res.getKind() >= co::TK_INT8 and res.getKind() <= co::TK_DOUBLE )
+				if( res.getKind() >= co::TK_INT8 && res.getKind() <= co::TK_DOUBLE )
 					status = res.get<int>();
 				else
 					std::cout << "Method returned " << res << "\n";
