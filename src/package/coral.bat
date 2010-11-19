402078224e79b36f3f45ad5b7e0bb7422bc24a00
Thiago Bastos <tabastos@gmail.com>
2010-11-19 13:13:00 -0200

    Renamed the launcher executable to solve the problems with PDB/CMake/Windows.
    The launcher was renamed from 'coral' to 'launcher'. This should not affect anyone using the 'coral' script, which was not renamed.

--- src/package/coral.bat
+++ src/package/coral.bat
@@ -4,15 +4,15 @@ set CORAL_ROOT=%~dp0
 set CORAL_PATH=%CORAL_ROOT%\modules;%CORAL_PATH%
 set PATH=%CORAL_ROOT%\lib;%PATH%
 
-IF EXIST "%CORAL_ROOT%\bin\coral.exe" (
-set CORAL_LAUNCHER=%CORAL_ROOT%\bin\coral.exe
+IF EXIST "%CORAL_ROOT%\bin\launcher.exe" (
+set CORAL_LAUNCHER=%CORAL_ROOT%\bin\launcher.exe
 ) ELSE (
-set CORAL_LAUNCHER=%CORAL_ROOT%\bin\coral_debug.exe
+set CORAL_LAUNCHER=%CORAL_ROOT%\bin\launcher_debug.exe
 )
 
 IF EXIST "%CORAL_LAUNCHER%" (
 "%CORAL_LAUNCHER%" %*
 ) ELSE (
-echo ERROR: the lancher executable is missing (either bin/coral.exe or bin/coral_debug.exe)
+echo ERROR: could not find the launcher executable (tried bin/launcher.exe and bin/launcher_debug.exe)
 exit /B 1
 )
